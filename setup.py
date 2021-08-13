6f4f62ed200bb721ae94d1428b2968146963f5b9
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2021-08-13 15:41:27 +0200

    add readme to pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from distutils.core import setup
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 setup(
-  name = 'easy-exchange-rates',         # How you named your package folder (MyLib)
-  packages = ['easy-exchange-rates'],   # Chose the same as "name"
+  name = 'easy_exchange_rates',         # How you named your package folder (MyLib)
+  packages = ['easy_exchange_rates'],   # Chose the same as "name"
   version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Easy abstraction to get currency exchange data from https://exchangerate.host/ to a pandas dataframe',   # Give a short description about your library
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'Odd Gunnar Aspaas',                   # Type in your name
   author_email = 'odd.learning@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/oddaspa/easy-exchange-rates',   # Provide either the link to your github or to your website
